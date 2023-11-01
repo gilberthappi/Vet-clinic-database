84da45e15613de882a66a13c090c7737359d4f1d
gilberthappi <gdushimimana6@gmail.com>
2023-11-01 15:59:10 +0200

    Update files for query_multiple_tables

--- schema.sql
+++ schema.sql
@@ -1,12 +1,30 @@
 /* Database schema to keep the structure of entire database. */
+DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS species;
 
-CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100),
-    species VARCHAR(100),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL,
-    PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS owners (
+    id SERIAL PRIMARY KEY NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    age NUMERIC NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS species (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS animals (
+    id SERIAL PRIMARY KEY NOT NULL,
+    species_id INT,
+    owners_id INT,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts NUMERIC NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL,
+    FOREIGN KEY (species_id)
+      REFERENCES species (id),
+    FOREIGN KEY (owners_id)
+      REFERENCES owners (id)
 );
