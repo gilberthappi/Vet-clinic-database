ebfac453cf4d7f55500b370113984c1c3fbf5ebe
gilberthappi <gdushimimana6@gmail.com>
2023-11-01 22:46:03 +0200

    Update files for query_multiple_tables

--- queries.sql
+++ queries.sql
@@ -1,42 +1,78 @@
--- What animals belong to Melody Pond?
-SELECT animals.name AS animal_name, owners.full_name AS owner_name
-FROM animals
-INNER JOIN owners ON animals.owner_id = owners.id
+/*Queries that provide answers to the questions from all projects.*/
+
+SELECT * from animals WHERE name LIKE '%mon';
+SELECT * from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
+SELECT * from animals WHERE escape_attempts < 3;
+SELECT * from animals WHERE name IN ('Agumon', 'Pikachu');
+SELECT name, escape_attempts from animals WHERE weight_kg > 10.5;
+SELECT * from animals WHERE neutered = true;
+SELECT * from animals WHERE name != 'Gabumon';
+SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT species from animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS null;
+COMMIT;
+SELECT name, species from animals;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+SELECT * from animals;
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT delete_after_2022;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK to delete_after_2022;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+SELECT COUNT(*) AS "No. of animals" FROM animals;
+SELECT COUNT(*) AS "No. of animals never scaped" FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) AS "Animals average weight" FROM animals;
+SELECT name, escape_attempts FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE  date_of_birth BETWEEN '1990-01-01' AND '2000-01-01' GROUP BY species;
+
+SELECT animals.name
+FROM animals 
+JOIN owners ON owners.id = animals.owner_id
 WHERE owners.full_name = 'Melody Pond';
 
--- List of all animals that are Pokemon (their type is Pokemon).
-SELECT animals.name AS animal_name, species.name AS species_name
-FROM animals
-INNER JOIN species ON animals.species_id = species.id
+SELECT animals.name
+FROM animals 
+JOIN species ON species.id = animals.species_id
 WHERE species.name = 'Pokemon';
 
--- List all owners and their animals, including those who don't own any animals.
-SELECT owners.full_name AS owner_name, COALESCE(animals.name, 'No animals') AS animal_name
-FROM owners
-LEFT JOIN animals ON owners.id = animals.owner_id;
+SELECT animals.name, owners.full_name
+FROM animals 
+RIGHT JOIN owners ON owners.id = animals.owner_id;
 
--- How many animals are there per species?
-SELECT species.name AS species_name, COUNT(animals.name) AS animal_count
-FROM species
-LEFT JOIN animals ON species.id = animals.species_id
+SELECT count(animals), species.name
+FROM animals 
+JOIN species ON species.id = animals.species_id
 GROUP BY species.name;
 
--- List all Digimon owned by Jennifer Orwell.
-SELECT animals.name AS animal_name
-FROM animals
-INNER JOIN owners ON animals.owner_id = owners.id
-INNER JOIN species ON animals.species_id = species.id
-WHERE owners.full_name = 'Jennifer Orwell' AND species.name = 'Digimon';
+SELECT animals.name, species.name, owners.full_name
+FROM animals 
+JOIN species ON species.id = animals.species_id
+JOIN owners ON owners.id = animals.owner_id
+WHERE species.name = 'Digimon' AND owners.full_name = 'Jennifer Orwell';
 
--- List all animals owned by Dean Winchester that haven't tried to escape.
-SELECT animals.name AS animal_name, owners.full_name AS owner_name
-FROM animals
-INNER JOIN owners ON animals.owner_id = owners.id
+SELECT animals.name, owners.full_name
+FROM animals 
+JOIN owners ON owners.id = animals.owner_id
 WHERE owners.full_name = 'Dean Winchester' AND animals.escape_attempts = 0;
 
--- Who owns the most animals?
-SELECT owners.full_name AS owner_name, COUNT(animals.name) AS animal_count
-FROM owners
-LEFT JOIN animals ON owners.id = animals.owner_id
+SELECT owners.full_name, COUNT(animals)
+FROM animals
+JOIN owners ON owners.id = animals.owner_id
 GROUP BY owners.full_name
-ORDER BY COUNT(animals.name) DESC;
+ORDER BY COUNT(animals) DESC LIMIT 1;
