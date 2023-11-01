6b89469790af65bb9bdd9df8b8c94e06e8f01eb7
gilberthappi <gdushimimana6@gmail.com>
2023-11-01 14:45:46 +0200

    Update and Delete animals

--- schema.sql
+++ schema.sql
@@ -3,6 +3,7 @@
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100),
+    species VARCHAR(100),
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
