15d76d4a183830977093e8f4d328908aeb37cd17
gilberthappi <gdushimimana6@gmail.com>
2023-11-01 12:09:59 +0200

    Update

--- data.sql
+++ data.sql
@@ -5,3 +5,4 @@ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) 
   ('Gabumon', '2018-11-15', 2, true, 8),
   ('Pikachu', '2021-01-07', 1, false, 15.04),
   ('Devimon', '2017-05-12', 5, true, 11);
+  
