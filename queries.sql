23801daaff39dd31a46263935398305c4c49bc1d
gilberthappi <gdushimimana6@gmail.com>
2023-11-01 23:33:55 +0200

    Update files for join_table

--- queries.sql
+++ queries.sql
@@ -75,4 +75,66 @@ SELECT owners.full_name, COUNT(animals)
 FROM animals
 JOIN owners ON owners.id = animals.owner_id
 GROUP BY owners.full_name
+
 ORDER BY COUNT(animals) DESC LIMIT 1;
+
+SELECT animals.name, visits.visit_date
+FROM animals
+JOIN visits ON visits.animal_id = animals.id
+JOIN vets ON vets.id = visits.vet_id
+WHERE vets.name = 'William Tatcher'
+ORDER BY visits.visit_date DESC LIMIT 1;
+
+SELECT COUNT(animals)
+FROM animals
+JOIN visits ON visits.animal_id = animals.id
+JOIN vets ON vets.id = visits.vet_id
+WHERE vets.name = 'Stephanie Mendez';
+
+SELECT vets.name, species.name
+FROM vets
+FULL JOIN specializations ON specializations.vet_id = vets.id
+FULL JOIN species ON species.id = specializations.species_id;
+
+SELECT animals.name, visits.visit_date
+FROM animals
+JOIN visits ON visits.animal_id = animals.id
+JOIN vets ON vets.id = visits.vet_id
+WHERE vets.name = 'Stephanie Mendez'
+AND visits.visit_date BETWEEN '2020-04-01' AND '2020-08-30';
+
+SELECT animals.name, COUNT(visits)
+FROM animals
+join visits on visits.animal_id = animals.id
+GROUP BY animals.name
+ORDER BY COUNT(visits) DESC LIMIT 1;
+
+SELECT animals.name, visits.visit_date
+FROM animals
+JOIN visits ON visits.animal_id = animals.id
+JOIN vets ON vets.id = visits.vet_id
+WHERE vets.name = 'Maisy Smith'
+ORDER BY visits.visit_date LIMIT 1;
+
+SELECT animals.name, vets.name, visits.visit_date
+FROM animals
+JOIN visits ON visits.animal_id = animals.id
+JOIN vets ON vets.id = visits.vet_id
+ORDER BY visits.visit_date DESC LIMIT 1;
+
+SELECT vets.name, COUNT (visits)
+from vets
+FULL JOIN Specializations ON Specializations.vet_id = vets.id
+FULL JOIN species ON species.id = specializations.species_id
+JOIN visits ON visits.vet_id = vets.id
+WHERE species.name IS NULL
+GROUP BY vets.name;
+
+SELECT vets.name, species.name, COUNT(species)
+from vets
+JOIN visits ON visits.vet_id = vets.id
+JOIN animals ON animals.id = visits.animal_id
+JOIN species ON species.id = animals.species_id
+WHERE vets.name = 'Maisy Smith'
+GROUP BY vets.name, species.name
+ORDER BY COUNT(species) DESC LIMIT 1;
--- schema.sql
+++ schema.sql
@@ -1,13 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
+DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100),
-    date_of_birth DATE,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL,
-    PRIMARY KEY(id)
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  date_of_birth DATE,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL,
+  PRIMARY KEY(id)
 );
 
 ALTER TABLE animals
@@ -43,3 +44,36 @@ ALTER TABLE animals
     FOREIGN KEY (owner_id)
     REFERENCES owners(id)
     ON DELETE CASCADE;
+
+DROP TABLE IF EXISTS vets;
+CREATE TABLE vets (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100),
+  age INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS specializations;
+CREATE TABLE specializations (
+	species_id INT,
+	vet_id INT,
+  PRIMARY KEY (species_id, vet_id),
+  CONSTRAINT fk_species 
+    FOREIGN KEY (species_id) REFERENCES species (id) ON DELETE CASCADE,
+  CONSTRAINT fk_vets
+	  FOREIGN KEY (vet_id) REFERENCES vets (id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS visits;
+CREATE TABLE visits (
+  id INT GENERATED ALWAYS AS IDENTITY,
+	animal_id INT,
+	vet_id INT,
+  visit_date DATE,
+  PRIMARY KEY (id),
+	CONSTRAINT fk_animals 
+    FOREIGN KEY (animal_id) REFERENCES animals (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT fk_vets_visits
+	  FOREIGN KEY (vet_id) REFERENCES vets (id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
